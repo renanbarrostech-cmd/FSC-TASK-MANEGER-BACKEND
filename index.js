e9ebb73be0347d351d2682595ed4a6c0d71c2507
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-23 20:30:27 -0300

    feat: add route to delete a task

--- index.js
+++ index.js
@@ -19,13 +19,31 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
-app.post("/task", async (req, res) => {
+app.post("/tasks", async (req, res) => {
     try {
         const newTask = new TaskModel(req.body);
 
         await newTask.save();
 
         res.status(201).send(newTask);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const taskToDelete = await TaskModel.findById(taskId);
+
+        if (!taskToDelete) {
+            return res.status(500).send("Essa tarefa não foi encontrada");
+        }
+
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+        res.status(200).send(deletedTask);
     } catch (error) {
         res.status(500).send(error.message);
     }
