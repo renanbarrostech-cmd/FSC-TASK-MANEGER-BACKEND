921fe2f871bcf1cf15b1627d14366526bf70abe0
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-26 10:14:19 -0300

    feat: move get tasks endpoint to a controller

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const TaskModel = require("../model/task.model");
+
+const TaskController = require("../controllers/task.controller");
+const TaskModel = require("../models/task.model");
 
 const router = express.Router();
 
 router.get("", async (req, res) => {
-    try {
-        const task = await TaskModel.find({});
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).getTasks();
 });
 
 router.get("/:id", async (req, res) => {
